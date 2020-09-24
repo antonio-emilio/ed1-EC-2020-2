bd3c4c23f8925516a6231b609d49224b6328e754
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-09-23 21:38:39 -0300

    definindo estrutura base para o projeto

---
+++ Project/src/main.c
@@ -0,0 +1,19 @@
+# include <ncurses.h>
+
+
+int main(){
+	// inicializa a tela
+	// prepara o buffer de memoria limpa a tela
+	initscr();
+	
+	// recarrega a tela para dar load no que esta
+	// na memoria
+	refresh();
+
+	getch();
+	
+
+	// desaloca a memoria e termina ncurses
+	endwin();
+	return 0;
+}
