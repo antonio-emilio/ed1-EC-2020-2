98b2284f085c3575c7c011e8c1a0d9f7a9ca3bf3
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-09-28 12:46:03 -0300

    label basic structure define

--- Project/src/Label.h
+++ Project/src/Label.h
@@ -1,16 +1,32 @@
 #ifndef __LABEL_H__
 #define __LABEL_H__ 
 
+#include <ncurses.h>
+#include <stdlib.h>
 
-#define LABEL_NAME_SIZE
 
-typedef char Name;
+#define LABEL_NAME_SIZE 20
+
+typedef char LabelName[LABEL_NAME_SIZE];
+
 typedef int Color;
 
 typedef struct Label{ 
+    LabelName name;
     Color color;
-    Name name;
 
 } Label_t;
+
+Label_t* createLabel(Color color, LabelName name);
+
+void deleteLabel(Label_t* l);
+
+void setColor(Label_t* l, Color c);
+
+void setLabelName(Label_t* l, LabelName name);
+
+Color getColor(Label_t* l);
+
+char* getLabelName(Label_t* l);
 
 #endif
