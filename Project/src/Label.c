16a57327eb2b43009a6b9c283dd906ffc718d961
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-09-30 08:19:01 -0300

    adicionando verificação de disponibilidade de cores no construtor de labels e outras pequenas mudanças

--- Project/src/Label.c
+++ Project/src/Label.c
@@ -8,47 +8,66 @@ Label_t* createLabel(Color color, LabelName name){
     if(color >= NUMB_COLORS || strlen(name) > LABEL_NAME_SIZE)
         return (Label_t*) INVALID_INPUT_EXCEPTION; 
 
+    // verifica se a cor já não está sendo usada 
+    if(!AVAILABLE_COLORS[color])
+        return (Label_t*) UNAVAILABLE_COLOR_EXCEPTION;
 
+
+    // aloca memória para a nova label  
     Label_t* new_label = (Label_t*) malloc(sizeof(Label_t));
     if(!new_label) return (Label_t*) MEMORY_ALOCATION_EXCEPTION;
 
-    new_label->color = color;
-    strcpy(new_label->name, name);
+    // atribui os valores passados a nova label
+    setColor(new_label, color);
+    setLabelName(new_label, name);
 
 
     return new_label;
 }
 
 
+
+
 Exception deleteLabel(Label_t* l){
+    if(!l) return INVALID_INPUT_EXCEPTION;
+
     free(l);
+    return SUCCESS;
 }
 
 
-Exception setColor(Label_t* l, Color c){
-    if(c >= NUMB_COLORS)
-        return INVALID_INPUT_EXCEPTION;
 
+
+
+// _______setters__________
+
+
+Exception setColor(Label_t* l, Color c){
+    if(c >= NUMB_COLORS) return INVALID_INPUT_EXCEPTION;
 
         l->color = c;
+        return SUCCESS;
 }
 
 
 Exception setLabelName(Label_t* l, LabelName name){
-    if(strlen(name) > LABEL_NAME_SIZE)
-        return INVALID_INPUT_EXCEPTION;
-
+    if(strlen(name) > LABEL_NAME_SIZE) return INVALID_INPUT_EXCEPTION;
 
         strcpy(l->name, name);
+        return SUCCESS;
 }
 
+
+// _______gettters________
+
 Color getColor(Label_t* l){
-    return l ? l->color : INVALID_INPUT_EXCEPTION;
+    return l ? l->color : (Color) INVALID_INPUT_EXCEPTION;
 }
+
 
 char* getLabelName(Label_t* l){
 
-    return l ? l->name :  INVALID_INPUT_EXCEPTION;
+    return l ? l->name :  (char*) INVALID_INPUT_EXCEPTION;
 
 }
 
