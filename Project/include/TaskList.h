972dd6c04be71649a017bf92bf87b0a5f1d0cf97
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-09-29 21:53:52 -0300

    estrutura basica de tasklist

--- Project/include/TaskList.h
+++ Project/include/TaskList.h
@@ -1,4 +1,16 @@
 #ifndef __TASK_LIST_H__
+
 #define __TASK_LIST_H__ 
+
+#include "Task.h"
+
+
+typedef struct Element{ 
+    Task_t task;
+    struct Element* next;
+} Element_t;
+
+typedef Element_t* TaskList_t; 
+
 
 #endif
