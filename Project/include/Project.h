68cb790c3b50101ead77b454cba8d5a201c8228d
Iscava <victor_eduardo29@hotmail.com>
2020-10-06 16:46:44 -0300

    atualização da estrutura do projeto

--- Project/include/Project.h
+++ Project/include/Project.h
@@ -3,4 +3,42 @@
 
 #include "projectOrganizer.h"
 
+// Project List struct
+typedef struct Project_List
+{
+    int id;
+    struct Project project;
+    struct Project next_project;
+} Project_List_t;
+
+typedef Project_List_t *ProjectList;
+
+// Project struct
+typedef struct Element
+{
+    Title title;
+    Description description;
+    struct Collaborator collaborator;
+} Element_t;
+
+typedef Element_t *Project;
+
+// Collaborator struct
+typedef struct Collaborator_Profile
+{
+    Name name;
+    Email email;
+    Bio bio;
+    struct Collaborator_Profile *next;
+} Collaborators_Profile_t;
+
+typedef Collaborators_t *Collaborator;
+
+// Start a project
+
+Project creteProject();
+
+
+
+
 #endif
--- Project/include/projectOrganizer.h
+++ Project/include/projectOrganizer.h
@@ -17,15 +17,22 @@
 #define True 1
 #define False 0 
 
-// tamanhos máximos de título e descrição
+// tamanhos máximos
 #define TITLE_SIZE 256
-#define DESCRIPTION_SIZE 1000
+#define DESCRIPTION_SIZE 1024
+
+#define NAME_SIZE 64
+#define EMAIL_SIZE 64
+#define BIO_SIZE 512
 
 typedef int boolean;
 
 // char* com os tamanhos específicos de title e description
 typedef char Description[DESCRIPTION_SIZE];
 typedef char Title[TITLE_SIZE]; 
+typedef char Name[NAME_SIZE]; 
+typedef char Email[EMAIL_SIZE]; 
+typedef char Bio[BIO_SIZE]; 
 
 
 
