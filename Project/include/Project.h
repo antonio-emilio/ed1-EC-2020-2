d5ce6ef27a6096e556e31ebeebee569112126b2e
Iscava <victor_eduardo29@hotmail.com>
2020-10-16 18:00:33 -0300

    Atualizando project

--- Project/include/Project.h
+++ Project/include/Project.h
@@ -4,40 +4,72 @@
 #include "projectOrganizer.h"
 
 // Project List struct
-typedef struct Project_List
+typedef struct Project_List_Element
 {
     int id;
     struct Project project;
-    struct Project next_project;
-} Project_List_t;
+    struct Project_List_Element *next_project;
+} Project_List_Element_t;
 
-typedef Project_List_t *ProjectList;
+typedef Project_List_Element_t *ProjectListElement;
+
+ProjectListElement firstProject = NULL;
 
 // Project struct
-typedef struct Element
+typedef struct Project_Element
 {
     Title title;
     Description description;
-    struct Collaborator collaborator;
-} Element_t;
+    struct CollaboratorsList collaboratorListElement; // Ponteiro para o primeiro elemento da lista de colaboradores
+} Project_Element_t;
 
-typedef Element_t *Project;
+typedef Project_Element_t *Project;
+
+
+// Collaborator List struct
+typedef struct Collaborators_List
+{
+    int id;
+    struct Collaborator collaborator;  // Perfil do colaborador
+    struct Collaborators_List *next_collaborator;   // Ponteiro para a próxima struct da lista de colaborador com o ponteiro do próximo colaborador da lista
+} Collaborators_List_t;
+
+typedef Collaborators_List_t *CollaboratorsList;
 
 // Collaborator struct
-typedef struct Collaborator_Profile
+typedef struct Collaborator_Element
 {
     Name name;
     Email email;
     Bio bio;
-    struct Collaborator_Profile *next;
-} Collaborators_Profile_t;
+} Collaborator_Element_t;
 
-typedef Collaborators_t *Collaborator;
+typedef Collaborator_Element_t *Collaborator;
 
-// Start a project
 
-Project creteProject();
+// Functions
 
+Project createProject(Title title, Description description, CollaboratorsList collaboratorsList);
+
+void showProjects();
+
+void showProjectDetails(Project project);
+
+void showProjectCollaboratorsList(Project project);
+
+void showCollaboratorProfile(Project project, Name name);
+
+Collaborator createCollaborator(Name name, Email email, Bio bio);
+
+void pushProject(Project project);
+
+void deleteProject(Project project);
+
+void pushCollaborator(Project protect, Collaborator collaborator);
+
+void deleteCollaborator(Project protect, Collaborator collaborator);
+
+Collaborator searchCollboratorByName(Name name);
 
 
 
