e29cae4b5225c2c5f386757295a8c7cc08201d1e
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-10-18 12:38:30 -0300

    working on the display functions of task

--- Project/include/Task.h
+++ Project/include/Task.h
@@ -1,50 +1,58 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
-/*
-    Uma task contém todas as informações de uma
-    determinada atividade. 
-*/
-#include "projectOrganizer.h"
+
+#include "ProjectOrganizer.h"
 
 #include "Label.h"
 
-// número máximo de labels que uma task pode ter 
+
 #define LABELS_MAX 4 
 
-#define TASK_HEIGHT 6
-#define TASK_WIDTH 30
 
-typedef WINDOW* TASK_WIN; 
+#define TASK_DISPLAY_WIDTH 20 
+#define TASK_DISPLAY_HEIGHT 5
+
+
 
 typedef struct Task{
 
     Title title;
     Description description;
 
-    Label_t labels[LABELS_MAX];
+
+    Label_t* labels[LABELS_MAX];
     int numb_labels;
+
+
+    WINDOW* win;
 
 } Task_t;
 
-/* constructor */
+
 Task_t* createTask(char* title, char* description);
 
-/* destructor */
+
 Exception deleteTask(Task_t* t);
 
-/* adiciona uma label para a task especifica */
+
 Exception addLabel(Task_t* t, Label_t* label);
 
 
-// setters
+Exception show(Task_t* t, unsigned int y, unsigned x);
+
+
+boolean isDisplayed(Task_t* t);
+
+
 Exception setDescription(Task_t* t, char* d);
+
 
 Exception setTitle(Task_t* t, char* title);
 
 
-// getters
 char* getDescription(Task_t* t);
+
 
 char* getTitle(Task_t* t);
 
--- Project/src/Task.c
+++ Project/src/Task.c
@@ -1,22 +1,31 @@
 #include "Task.h" 
 
 
+
+
 Task_t* createTask(char* title, char* description){
-    if(strlen(title) >= TITLE_SIZE || strlen(description) >= DESCRIPTION_SIZE)
+
+    if(strlen(title) >= TASK_DISPLAY_WIDTH)
         return (Task_t*) INVALID_INPUT_EXCEPTION;
+
 
     // aloca memória 
     Task_t* new_task = (Task_t*) malloc(sizeof(Task_t));
     if(!new_task) return (Task_t*) MEMORY_ALOCATION_EXCEPTION;
 
+
     // atribui os valores 
     setDescription(new_task, description);
     setTitle(new_task, title);
+
     new_task->numb_labels = 0;
+    new_task->win = NULL;
 
 
     return new_task;
 }
+
+
 
 
 Exception deleteTask(Task_t* t){
@@ -27,39 +36,97 @@ Exception deleteTask(Task_t* t){
 }
 
 
-/* adiciona uma label para a task especifica */
+
+
 Exception addLabel(Task_t* t, Label_t* label){
     if(!t || !label || t->numb_labels >= LABELS_MAX) return INVALID_INPUT_EXCEPTION;
 
-    t->labels[t->numb_labels++] = *label;
+    t->labels[t->numb_labels++] = label;
 }
 
 
-// settters
+
+
+Exception show(Task_t* t, unsigned int y, unsigned x){
+    if(!t) return INVALID_INPUT_EXCEPTION; 
+
+
+    t->win = newwin(TASK_DISPLAY_HEIGHT, TASK_DISPLAY_WIDTH, y, x);    
+    refresh();
+
+    box(t->win, 0, 0);
+
+
+    int title_len = strlen(t->title);
+
+    wmove(t->win, 0, (TASK_DISPLAY_WIDTH - title_len)/2);
+    wprintw(t->win, "%s", t->title);
+
+    wrefresh(t->win);
+
+}
+
+
+
+
+Exception hide(Task_t* t){
+    if(!t) return INVALID_INPUT_EXCEPTION; 
+
+    //TODO: implement
+    
+}
+
+
+
+
+boolean isDisplayed(Task_t* t){
+    return (boolean) t->win;
+}
+
+
+
 
 Exception setDescription(Task_t* t, char* d){
+
     if(!t || strlen(d) >= DESCRIPTION_SIZE) return INVALID_INPUT_EXCEPTION;
 
+
     strcpy(t->description, d);
+
+
     return SUCCESS;
 }
+
+
 
 
 Exception setTitle(Task_t* t, char* title){
+
     if(!t || strlen(title) >= TITLE_SIZE) return INVALID_INPUT_EXCEPTION;
 
+
     strcpy(t->title, title);
+
+
     return SUCCESS;
 }
 
 
-// getters
+
+
 char* getDescription(Task_t* t){
+
     return !t ? (char*) INVALID_INPUT_EXCEPTION : t->description;
 }
 
 
+
+
 char* getTitle(Task_t* t){
+
     return !t ? (char* ) INVALID_INPUT_EXCEPTION : t->title;
 }
+
+
+
 
