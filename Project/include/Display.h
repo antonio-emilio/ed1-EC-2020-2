3779f7bdc56cbf50e3b96f061c635046dea40698
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-10-27 13:28:09 -0300

    criando componente de display menu de seleção

--- Project/include/Display.h
+++ Project/include/Display.h
@@ -2,8 +2,14 @@
 #define __DISPLAY_H__
 
 #include <ncurses.h>
+#include <string.h>
 
-// this func values need to be hard coded unfortunaly :c
+#define BANNER_COLOR 100 
+
+bool IS_RESIZED;
+
+int selectWin(char** options, char* question, int num_options, int y, int x, unsigned int width,unsigned int height, int default_choice);
+
 void printBanner(int y, int x);
 
 #endif 
--- Project/src/Display.c
+++ Project/src/Display.c
@@ -31,10 +31,98 @@ void printBanner(int y, int x){
         "                     `Y88P`                                                 "
 
     };
+    init_pair(BANNER_COLOR, COLOR_CYAN, COLOR_BLACK); 
 
+    x -= strlen(banner_lines[0]); 
 
+    attron(COLOR_PAIR(BANNER_COLOR));
 
     for(int i = 0; i < 22; i++)
         mvprintw(y++, x,"%s", banner_lines[i]);
+
+    attroff(COLOR_PAIR(BANNER_COLOR));
         
+}
+
+
+int selectWin(char** options, char* question, int num_options, int y, int x, unsigned int width,unsigned int height,  int default_choice){
+
+    int menu_width;
+    int menu_height;
+
+    if(!width || !height) 
+    {
+        menu_width = strlen(question) + 10;
+        menu_height = num_options + 4;
+    }
+    else
+    {
+        menu_width  = width;
+        menu_height = height;
+    }
+
+    // define the location considering the size of the win
+    int menu_x = x - menu_width;
+    int menu_y = y - menu_height;
+
+    menu_x = menu_x < 0 ? 0 : menu_x;
+    menu_y = menu_y < 0 ? 0 : menu_y;
+
+    WINDOW* menu_win = newwin(menu_height, menu_width, menu_y , menu_x); 
+
+    box(menu_win, 0, 0);
+
+    int question_length = strlen(question);
+
+    mvwprintw(menu_win, 0, (menu_width - question_length)/2, question);
+
+    keypad(menu_win, TRUE );
+
+    refresh();
+
+    int selected = default_choice;
+    bool bye = FALSE; 
+
+    while(!bye)
+    {
+        for(int i = 0; i < num_options; i++)
+        {
+            if(i == selected)
+                wattron(menu_win, A_REVERSE); 
+
+            mvwprintw(menu_win, 2 + i, 1, options[i]);
+            wattroff(menu_win, A_REVERSE);
+        } 
+        wrefresh(menu_win);
+
+        int choice = getch();
+
+        switch(choice)
+        {
+            case 65: // key up
+                selected--;
+                break;
+            case 66: // key down
+                selected++;
+                break;
+            case 10: // enter
+                bye = TRUE;
+                break;
+        }
+
+        if(selected < 0)
+            selected = num_options - 1;
+
+        if(selected > num_options - 1)
+            selected = 0;
+
+        wrefresh(menu_win);
+    }
+
+    delwin(menu_win);
+
+    return selected;
+
+
+
 }
