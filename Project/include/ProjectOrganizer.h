1b130562b09ccda7d3384f5476d30394e4f063e0
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-10-27 13:27:34 -0300

    abandonando a forma de tratar exceptions e adotando a forma mais comum usada em C

--- Project/include/ProjectOrganizer.h
+++ Project/include/ProjectOrganizer.h
@@ -1,8 +1,7 @@
 #ifndef __PROJECTORG_H__
 #define __PROJECTORG_H__
 
-
-
+/* Global definitions, functions and var */
 #include <ncurses.h>
 
 #include <stdlib.h>
@@ -10,24 +9,25 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-#include "Exceptions.h"
 #include "Display.h"
+
 
 #define True 1
 #define False 0 
+
+#define ERROR -1
+#define OK 0
 
 #define DESCRIPTION_SIZE 1000
 #define NAME_SIZE 1
 #define EMAIL_SIZE 1
 #define BIO_SIZE 1
 
-typedef int boolean;
 
 typedef char Description[DESCRIPTION_SIZE];
 typedef char Name[NAME_SIZE]; 
 typedef char Email[EMAIL_SIZE]; 
 typedef char Bio[BIO_SIZE]; 
-
 
 
 #endif 
